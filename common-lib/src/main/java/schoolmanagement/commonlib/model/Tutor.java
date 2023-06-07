9d4103c37b6b55764197e336c51ce3ee29f04104
Djordje Ivanovic <ivanovic.djordje2019@gmail.com>
2023-06-08 00:08:58 +0200

    Documented whole model

--- common-lib/src/main/java/schoolmanagement/commonlib/model/Tutor.java
+++ common-lib/src/main/java/schoolmanagement/commonlib/model/Tutor.java
@@ -4,65 +4,167 @@ import java.io.Serializable;
 import java.util.List;
 import java.util.Objects;
 
+/**
+ * The {@code Tutor} class represents a tutor in the language learning system. A
+ * tutor is responsible for teaching courses and specializes in certain
+ * languages. It contains information such as the tutor's ID, first name, last
+ * name, and the list of languages they specialize in.
+ *
+ * @author Djordje Ivanovic
+ * 
+ */
 public class Tutor implements Serializable {
 
+	/**
+	 * The unique ID of the tutor.
+	 */
 	private Long id;
+
+	/**
+	 * The first name of the tutor.
+	 */
 	private String firstName;
+
+	/**
+	 * The last name of the tutor.
+	 */
 	private String lastName;
+
+	/**
+	 * The list of languages that the tutor specializes in. Each tutor can teach
+	 * multiple languages.
+	 *
+	 * @see Language
+	 */
 	private List<Language> languages;
 
+	/**
+	 * Constructs a {@code Tutor} object with the specified first name, last name,
+	 * and languages they specialize in.
+	 *
+	 * @param firstName The first name of the tutor.
+	 * @param lastName  The last name of the tutor.
+	 * @param languages The list of languages the tutor specializes in.
+	 * @see Language
+	 */
 	public Tutor(String firstName, String lastName, List<Language> languages) {
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.languages = languages;
 	}
 
+	/**
+	 * Default constructor for the {@code Tutor} class. Creates a tutor object with
+	 * default values.
+	 */
 	public Tutor() {
 	}
 
+	/**
+	 * Retrieves the ID of the tutor.
+	 *
+	 * @return The ID of the tutor.
+	 */
 	public Long getId() {
 		return id;
 	}
 
+	/**
+	 * Sets the ID of the tutor.
+	 *
+	 * @param id The ID of the tutor.
+	 */
 	public void setId(Long id) {
 		this.id = id;
 	}
 
+	/**
+	 * Retrieves the first name of the tutor.
+	 *
+	 * @return The first name of the tutor.
+	 */
 	public String getFirstName() {
 		return firstName;
 	}
 
+	/**
+	 * Sets the first name of the tutor.
+	 *
+	 * @param firstName The first name of the tutor.
+	 */
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
 
+	/**
+	 * Retrieves the last name of the tutor.
+	 *
+	 * @return The last name of the tutor.
+	 */
 	public String getLastName() {
 		return lastName;
 	}
 
+	/**
+	 * Sets the last name of the tutor.
+	 *
+	 * @param lastName The last name of the tutor.
+	 */
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
 
+	/**
+	 * Retrieves the list of languages the tutor specializes in.
+	 *
+	 * @return The list of languages the tutor specializes in.
+	 * @see Language
+	 */
 	public List<Language> getLanguages() {
 		return languages;
 	}
 
+	/**
+	 * Sets the list of languages the tutor specializes in.
+	 *
+	 * @param languages The list of languages the tutor specializes in.
+	 * @see Language
+	 */
 	public void setLanguages(List<Language> languages) {
 		this.languages = languages;
 	}
 
+	/**
+	 * Returns the string representation of the tutor object. The string contains
+	 * the tutor's first name and last name.
+	 *
+	 * @return The string representation of the tutor object.
+	 */
 	@Override
 	public String toString() {
 		return firstName + " " + lastName;
 	}
 
+	/**
+	 * Returns the hash code value for the tutor object. The hash code is computed
+	 * based on the tutor's ID.
+	 *
+	 * @return The hash code value for the tutor object.
+	 */
 	@Override
 	public int hashCode() {
 		int hash = 3;
 		return hash;
 	}
 
+	/**
+	 * Indicates whether some other object is "equal to" this one. Two tutors are
+	 * considered equal if they have the same ID.
+	 *
+	 * @param obj The reference object with which to compare.
+	 * @return {@code true} if this tutor is the same as the obj argument;
+	 *         {@code false} otherwise.
+	 */
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj) {
--- common-lib/src/main/java/schoolmanagement/commonlib/model/User.java
+++ common-lib/src/main/java/schoolmanagement/commonlib/model/User.java
@@ -2,50 +2,118 @@ package schoolmanagement.commonlib.model;
 
 import java.io.Serializable;
 
+/**
+ * The User class represents a user in the system. It provides basic
+ * functionality for storing and retrieving user information.
+ * 
+ * @author Djordje Ivanovic
+ */
 public class User implements Serializable {
 
+	/**
+	 * The ID of the user.
+	 */
 	protected Long id;
+
+	/**
+	 * The username of the user.
+	 */
 	protected String username;
+
+	/**
+	 * The password of the user.
+	 */
 	protected String password;
 
+	/**
+	 * Constructs a User object with the specified ID, username, and password.
+	 *
+	 * @param id       The ID of the user.
+	 * @param username The username of the user.
+	 * @param password The password of the user.
+	 */
 	public User(Long id, String username, String password) {
 		this.id = id;
 		this.username = username;
 		this.password = password;
 	}
 
+	/**
+	 * Constructs a User object with the specified username and password.
+	 *
+	 * @param username The username of the user.
+	 * @param password The password of the user.
+	 */
 	public User(String username, String password) {
 		this.username = username;
 		this.password = password;
 	}
 
+	/**
+	 * Returns the ID of the user.
+	 *
+	 * @return The ID of the user.
+	 */
 	public Long getId() {
 		return id;
 	}
 
+	/**
+	 * Sets the ID of the user.
+	 *
+	 * @param id The ID of the user.
+	 */
 	public void setId(Long id) {
 		this.id = id;
 	}
 
+	/**
+	 * Returns the username of the user.
+	 *
+	 * @return The username of the user.
+	 */
 	public String getUsername() {
 		return username;
 	}
 
+	/**
+	 * Sets the username of the user.
+	 *
+	 * @param username The username of the user.
+	 */
 	public void setUsername(String username) {
 		this.username = username;
 	}
 
+	/**
+	 * Returns the password of the user.
+	 *
+	 * @return The password of the user.
+	 */
 	public String getPassword() {
 		return password;
 	}
 
+	/**
+	 * Sets the password of the user.
+	 *
+	 * @param password The password of the user.
+	 */
 	public void setPassword(String password) {
 		this.password = password;
 	}
 
-	@Override
-	public String toString() {
-		return "User{" + "id=" + id + ", username=" + username + ", password=" + password + '}';
-	}
-
+	/**
+     * Returns a string representation of the User object.
+     *
+     * @return A string representation of the User object.
+     */
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
