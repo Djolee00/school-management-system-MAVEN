7e2c8ff1b6b5e0d78e230d85846211cf9997617b
Djordje Ivanovic <ivanovic.djordje2019@gmail.com>
2023-06-08 13:46:39 +0200

    Documented enums

--- common-lib/src/main/java/schoolmanagement/commonlib/communication/Operation.java
+++ common-lib/src/main/java/schoolmanagement/commonlib/communication/Operation.java
@@ -2,23 +2,51 @@ package schoolmanagement.commonlib.communication;
 
 import java.io.Serializable;
 
+/**
+ * Enumeration of operations supported by the system.
+ *
+ * <p>
+ * This enumeration represents various operations that can be performed within
+ * the system.
+ * </p>
+ *
+ * <p>
+ * The available operations include:
+ * </p>
+ * <ul>
+ * <li>{@link #LOGIN} - Operation for performing login.</li>
+ * <li>{@link #GET_STUDENT_COURSES} - Operation for retrieving courses for a
+ * student.</li>
+ * <li>{@link #GET_STUDENT_UNSELECTED_COURSES} - Operation for retrieving
+ * unselected courses for a student.</li>
+ * <li>{@link #ENROLL_STUDENT_IN_COURSES} - Operation for enrolling a student in
+ * courses.</li>
+ * <li>{@link #GET_ALL_STUDENTS} - Operation for retrieving all students.</li>
+ * <li>{@link #GET_ALL_COURSES} - Operation for retrieving all courses.</li>
+ * <li>{@link #GET_ALL_LANGUAGES} - Operation for retrieving all languages.</li>
+ * <li>{@link #UPDATE_STUDENT_PERSONAL_INFO} - Operation for updating student's
+ * personal information.</li>
+ * <li>{@link #ADD_NEW_STUDENT} - Operation for adding a new student.</li>
+ * <li>{@link #UPDATE_COURSE} - Operation for updating a course.</li>
+ * <li>{@link #DELETE_COURSE} - Operation for deleting a course.</li>
+ * <li>{@link #ADD_COURSE} - Operation for adding a new course.</li>
+ * <li>{@link #GET_COURSE_GROUPS} - Operation for retrieving course groups.</li>
+ * <li>{@link #GET_LANGUAGE_TUTORS} - Operation for retrieving language
+ * tutors.</li>
+ * <li>{@link #GET_COURSE_STUDENTS} - Operation for retrieving students in a
+ * course.</li>
+ * <li>{@link #ADD_COURSE_GROUP} - Operation for adding a course group.</li>
+ * <li>{@link #UPDATE_COURSE_GROUP} - Operation for updating a course
+ * group.</li>
+ * <li>{@link #LOG_OUT} - Operation for logging out.</li>
+ * </ul>
+ *
+ * @see Serializable
+ * @author Djordje Ivanovic
+ */
 public enum Operation implements Serializable {
-    LOGIN,
-    GET_STUDENT_COURSES,
-    GET_STUDENT_UNSELECTED_COURSES,
-    ENROLL_STUDENT_IN_COURSES,
-    GET_ALL_STUDENTS,
-    GET_ALL_COURSES,
-    GET_ALL_LANGUAGES,
-    UPDATE_STUDENT_PERSONAL_INFO,
-    ADD_NEW_STUDENT,
-    UPDATE_COURSE,
-    DELETE_COURSE,
-    ADD_COURSE,
-    GET_COURSE_GROUPS,
-    GET_LANGUAGE_TUTORS,
-    GET_COURSE_STUDENTS, 
-    ADD_COURSE_GROUP,
-    UPDATE_COURSE_GROUP,
-    LOG_OUT
+	LOGIN, GET_STUDENT_COURSES, GET_STUDENT_UNSELECTED_COURSES, ENROLL_STUDENT_IN_COURSES, GET_ALL_STUDENTS,
+	GET_ALL_COURSES, GET_ALL_LANGUAGES, UPDATE_STUDENT_PERSONAL_INFO, ADD_NEW_STUDENT, UPDATE_COURSE, DELETE_COURSE,
+	ADD_COURSE, GET_COURSE_GROUPS, GET_LANGUAGE_TUTORS, GET_COURSE_STUDENTS, ADD_COURSE_GROUP, UPDATE_COURSE_GROUP,
+	LOG_OUT
 }
--- common-lib/src/main/java/schoolmanagement/commonlib/model/enums/Level.java
+++ common-lib/src/main/java/schoolmanagement/commonlib/model/enums/Level.java
@@ -1,8 +1,24 @@
 package schoolmanagement.commonlib.model.enums;
 
+
 /**
- * The Level enum represents language proficiency levels according to the Common European Framework of Reference for Languages (CEFR).
- * It includes six levels: A1, A2, B1, B2, C1, and C2.
+ * Enumeration of language proficiency levels.
+ *
+ * <p>This enumeration represents the different language proficiency levels according to the Common European Framework of Reference for Languages (CEFR).</p>
+ *
+ * <p>The available levels are:</p>
+ * <ul>
+ *   <li>{@link #A1} - Beginner (Breakthrough or Beginner)</li>
+ *   <li>{@link #A2} - Elementary (Waystage or Elementary)</li>
+ *   <li>{@link #B1} - Intermediate (Threshold or Pre-Intermediate)</li>
+ *   <li>{@link #B2} - Upper Intermediate (Vantage or Intermediate)</li>
+ *   <li>{@link #C1} - Advanced (Effective Operational Proficiency or Upper-Intermediate)</li>
+ *   <li>{@link #C2} - Proficient (Mastery or Advanced)</li>
+ * </ul>
+ *
+ * @see Serializable
+ * @see <a href="https://www.coe.int/en/web/common-european-framework-reference-languages">Common European Framework of Reference for Languages (CEFR)</a>
+ * @author Djordje Ivanovic
  */
 public enum Level {
     
