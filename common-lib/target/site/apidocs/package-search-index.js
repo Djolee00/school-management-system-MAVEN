f7b406a6022f527c1c50f93c81c28c11065d1ab4
Djordje Ivanovic <ivanovic.djordje2019@gmail.com>
2023-06-08 14:50:41 +0200

    Removed target folders

--- common-lib/target/site/apidocs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"schoolmanagement.commonlib.communication"},{"l":"schoolmanagement.commonlib.model"},{"l":"schoolmanagement.commonlib.model.enums"}];updateSearchResults();
