7735ee0b6ee56231908116d03843467bea316086
Djordje Ivanovic <ivanovic.djordje2019@gmail.com>
2023-06-10 21:53:49 +0200

    UserService documented

--- server/src/main/java/schoolmanagement/service/UserService.java
+++ server/src/main/java/schoolmanagement/service/UserService.java
@@ -4,7 +4,36 @@ import java.io.IOException;
 import java.sql.SQLException;
 import schoolmanagement.commonlib.model.User;
 
+/**
+ * The {@code UserService} interface represents a user service in the system. It
+ * provides methods for user authentication and related operations.
+ *
+ * <p>
+ * This interface defines the contract for classes that implement user
+ * authentication functionality. Implementing classes should provide an
+ * implementation for the {@link #login(String, String)} method.
+ * </p>
+ *
+ * <p>
+ * Implementation note: The authentication process may involve accessing a data
+ * source to validate the user credentials.
+ * </p>
+ *
+ *
+ * @author Djordje Ivanovic
+ * @see User
+ */
 public interface UserService {
 
+	/**
+	 * Logs in a user with the provided username and password.
+	 *
+	 * @param username the username of the user
+	 * @param password the password of the user
+	 * @return the logged-in {@link User} object, or {@code null} if the login is
+	 *         unsuccessful
+	 * @throws IOException  if an I/O error occurs while accessing the data source
+	 * @throws SQLException if an SQL error occurs while accessing the data source
+	 */
 	User login(String username, String password) throws IOException, SQLException;
 }
--- server/src/main/java/schoolmanagement/service/impl/UserServiceImpl.java
+++ server/src/main/java/schoolmanagement/service/impl/UserServiceImpl.java
@@ -10,18 +10,71 @@ import schoolmanagement.persistence.dao.UserDao;
 import schoolmanagement.persistence.pool.ConnectionPool;
 import schoolmanagement.service.UserService;
 
+/**
+ * The {@code UserServiceImpl} class is an implementation of the
+ * {@link UserService} interface. It provides user authentication functionality
+ * by interacting with the data source. This implementation relies on the
+ * UserDao, StudentDao, and AdminDao objects for data access operations.
+ * 
+ * <p>
+ * Implementation note: This class assumes the usage of a connection pool for
+ * efficient management of database connections. The connection pool can be
+ * obtained through the ConnectionPool.getInstance() method.
+ * </p>
+ * 
+ * 
+ * @author Djordje Ivanovic
+ * @see UserService
+ * @see UserDao
+ * @see StudentDao
+ * @see AdminDao
+ * @see ConnectionPool
+ */
 public class UserServiceImpl implements UserService {
 
-	private final UserDao userDao;
-	private final StudentDao studentDao;
-	private final AdminDao adminDao;
+	/**
+	 * Object for user-related data access operations
+	 */
+	private UserDao userDao;
+	/**
+	 * Object for student-related data access operations
+	 */
+	private StudentDao studentDao;
+	/**
+	 * Object for admin-related data access operations
+	 */
+	private AdminDao adminDao;
 
+	/**
+	 * Constructs a new {@code UserServiceImpl} object. Initializes the UserDao,
+	 * StudentDao, and AdminDao objects.
+	 */
 	public UserServiceImpl(UserDao userDao, StudentDao studentDao, AdminDao adminDao) {
 		this.userDao = userDao;
 		this.studentDao = studentDao;
 		this.adminDao = adminDao;
 	}
 
+	/**
+	 * Logs in a user with the provided username and password.
+	 *
+	 * <p>
+	 * This method retrieves a database connection from the connection pool and sets
+	 * it up for manual transaction management. It attempts to fetch a user from the
+	 * data source using the provided username and password. If a user is found, it
+	 * checks if the user is a student or an admin. The transaction is committed,
+	 * and the connection is released back to the connection pool upon successful
+	 * execution. If an I/O or SQL error occurs during the operation, the
+	 * transaction is rolled back, the connection is released, and the exception is
+	 * rethrown.
+	 * </p>
+	 *
+	 * @param username the username of the user
+	 * @param password the password of the user
+	 * @return the logged-in user, or null if the login is unsuccessful
+	 * @throws IOException  if an I/O error occurs while accessing the data source
+	 * @throws SQLException if an SQL error occurs while accessing the data source
+	 */
 	@Override
 	public User login(String username, String password) throws IOException, SQLException {
 		Connection connection = ConnectionPool.getInstance().getConnection();
